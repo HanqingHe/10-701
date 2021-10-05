059ade09850ab84856954a2285167f87e4ddf161
Francis He <francishe1996@gmail.com>
2021-10-05 14:22:04 -0400

    Added docstring to cleaner.py

--- HW2/cleaner.py
+++ HW2/cleaner.py
@@ -8,6 +8,17 @@ from HW2.util import split_xy
 
 
 def binary_encoder(dset: pd.DataFrame, features: list, to_replace: dict = 'YN') -> pd.DataFrame:
+    r"""Replace binary features with numbers
+
+    Args:
+        dset: pd.DataFrame
+        features: list
+            Names of binary features
+        to_replace: dict
+            Replacement rule
+    Returns:
+        pd.DataFrame
+    """
     if to_replace == 'YN':
         _to_replace = {col: {'Yes': 1, 'No': 0} for col in features}
     else:
@@ -16,6 +27,25 @@ def binary_encoder(dset: pd.DataFrame, features: list, to_replace: dict = 'YN') 
 
 
 def onehot_encoder(dset: pd.DataFrame, feature: str, sep: str = '') -> pd.DataFrame:
+    r"""One-hot encoder of categorical features
+
+    Args:
+        dset: pd.DataFrame
+        feature: str
+            Name of the categorical feature
+        sep: str
+            Separator between feature name and feature value
+            E.g.
+            For
+                feature name: 'ShelveLoc'
+                feature value: 'Good'
+                sep: ''
+            The name of the one-hot encoded feature column will be
+                'ShelveLocGood'
+
+    Returns:
+        pd.DataFrame
+    """
     feature_vals = dset[feature].value_counts().sort_index().index
     cat_cols = [sep.join([feature, val]) for val in feature_vals]
     # Split the dataset by columns
@@ -38,6 +68,17 @@ def onehot_encoder(dset: pd.DataFrame, feature: str, sep: str = '') -> pd.DataFr
 
 
 def standardize(train: pd.DataFrame, test: pd.DataFrame, ddof: int = 0) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    r"""Standardize features using training data
+
+    Args:
+        train: pd.DataFrame
+        test: pd.DataFrame
+        ddof: int
+            Degree of freedom
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]
+    """
     mu = train.mean()
     sigma = train.std(ddof=ddof)
     train_std = (train - mu) / sigma
@@ -59,18 +100,19 @@ if __name__ == '__main__':
     train_clean_path = os.path.join(dset_dirpath, train_clean_filename)
     test_clean_path = os.path.join(dset_dirpath, test_clean_filename)
 
+    # Load datasets
     train = pd.read_csv(train_path)
     test = pd.read_csv(test_path)
-
+    # Split features and targets
     X_train, y_train = split_xy(train, target)
     X_test, y_test = split_xy(test, target)
-
+    # Encode binary and categorical features
     X_train_encoded = onehot_encoder(binary_encoder(X_train, ['Urban', 'US']), 'ShelveLoc')
     X_test_encoded = onehot_encoder(binary_encoder(X_test, ['Urban', 'US']), 'ShelveLoc')
     X_train_clean, X_test_clean = standardize(X_train_encoded, X_test_encoded)
-
+    # Combine encoded features with targets
     train_clean = pd.concat([y_train, X_train_clean], axis=1)
     test_clean = pd.concat([y_test, X_test_clean], axis=1)
-
+    # Save encoded dataset to file
     train_clean.to_csv(train_clean_path, index=False)
     test_clean.to_csv(test_clean_path, index=False)
