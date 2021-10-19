32977c1ca2cb0130fdff7945d1b59a56189e6ae9
Francis He <francishe1996@gmail.com>
2021-10-19 06:34:30 -0400

    Added util.plot_confusion_matrix

--- HW3/util.py
+++ HW3/util.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import itertools
 from time import time
 from typing import Tuple
 
@@ -89,6 +90,45 @@ def plot_image(vector, out_f_name, label=None):
         plt.title(label)
     plt.axis('off')
     plt.savefig(f'{out_f_name}.png', bbox_inches='tight')
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.get_cmap('Blues')):
+    r"""Plot confusion matrix
+
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+
+    References:
+        https://scikit-learn.org/0.18/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 
 def plot_diff(x: np.ndarray, x_ref: np.ndarray,
